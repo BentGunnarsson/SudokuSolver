e0781bad8f762aff0a3938b804d9f361706b26c5
BentGunnarsson <bent19@ru.is>
2020-05-07 12:34:39 +0000

    readme instructions

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # SudokuSolver
+
 Simple sudoku solver, using recursion, with in-terminal visualizer
+
+To solve your sudoku puzzle:
+
+1. Fill in this format:
+
+zero_grid = [
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+2. Replace the "numeric grid" with the custom one you just made.
+3. Run the program and pick puzzle nr. 2
+4. Watch it go :)
+
+You can make it faster by removing line 66 but that makes it harder to follow.
--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,18 +1,6 @@
 import numpy as np
 import time
 
-basic_grid = [
-    [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    [0, 4, 9, 2, 0, 6, 0, 0, 7],
-]
-
 numeric_grid = [
     [0, 0, 0, 0, 1, 0, 0, 0, 0],
     [0, 0, 0, 3, 8, 7, 0, 9, 0],
@@ -23,6 +11,18 @@ numeric_grid = [
     [0, 8, 3, 0, 0, 9, 0, 4, 7],
     [6, 0, 0, 0, 0, 0, 0, 2, 0],
     [0, 2, 7, 1, 6, 0, 0, 0, 0],
+]
+
+basic_grid = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7],
 ]
 
 zero_grid = [
@@ -36,7 +36,6 @@ zero_grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
-
 
 def valid(grid, x, y, num):
     for i in range(9):
@@ -78,16 +77,20 @@ def solver(grid):
     solve(grid)
 
 def main_menu(grid):
+    print("You chose to solve this puzzle!")
     print(np.matrix(grid))
+    time.sleep(2)
+    print("Let's go!")
+    time.sleep(1)
     solver(grid)
 
 def main():
     print("Which would you like..")
-    num = input("Press 1 to solve a basic sudoku puzzle.\nPress 0 to solve a 0 matrix sudoku puzzle!\n")
+    num = input("Press 1 to test a basic sudoku puzzle.\nPress 0 to solve a 0 matrix sudoku puzzle!\nPress 2 for your own custom puzzle\n")
     try:
         int_num = int(num)
     except ValueError:
-        print("NUMBERS 0 OR 1 ONLY")
+        print("Numbers only..")
     if int_num == 1:
         main_menu(basic_grid)
     elif int_num == 2:
@@ -95,9 +98,8 @@ def main():
     elif int_num == 0:
         main_menu(zero_grid)
     else:
-        print("Invalid, retard")
+        print("Invalid")
 
     main()
-
 
 main()
